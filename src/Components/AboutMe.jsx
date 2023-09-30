0b7b1def3e66aada53475fec03b4e32e000ef090
Miguel Favio <miguelfavioqm@gmail.com>
2023-09-30 11:31:49 -0400

    Prueba 3d

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const AboutMe = () => {
   return (
     <section id="AboutMe" className="w-full h-screen">
-      SOBRE MI
+      <iframe
+        src="https://my.spline.design/interactivespaceshipscene-b057d4f88e22ed3a3f7c417e8785a1f2/"
+        className="bg-black w-full h-screen"
+      ></iframe>
     </section>
   );
 };
